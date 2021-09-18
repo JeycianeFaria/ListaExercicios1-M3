afca617127104125a64d06cdb9990efd17770f28
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-09-18 09:37:25 -0300

    Instanciando Objetos e atribuindo valor

--- src/br/com/zup/Exercicio1/Main.java
+++ src/br/com/zup/Exercicio1/Main.java
@@ -7,7 +7,22 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //instanciando objetos bola
+        Bola bolaFutsal = new Bola();
+        Bola bolaBasquete = new Bola();
 
+
+        //Atribindo valores a cada parametro dos Objetos
+        bolaFutsal.circunferencia = 61;
+        bolaFutsal.cor = "Branca e Azul";
+        bolaFutsal.marca = "Penalty";
+        bolaFutsal.velocidade = 100;
+
+
+        bolaBasquete.circunferencia = 75;
+        bolaBasquete.cor = "Laranja";
+        bolaBasquete.marca = "Wilson NCAA";
+        bolaBasquete.velocidade = 50;
 
     }
 
