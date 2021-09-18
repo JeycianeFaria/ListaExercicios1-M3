b1843745fe0fe5bfb13b5fb460803fd2fcba096d
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-09-18 09:35:26 -0300

    Classe Bola criada

---
+++ src/br/com/zup/Exercicio1/Bola.java
@@ -0,0 +1,10 @@
+package br.com.zup.Exercicio1;
+
+public class Bola {
+
+    double circunferencia;
+    double velocidade;
+    String cor;
+    String marca;
+
+}
--- src/br/com/zup/Exercicio1/Main.java
+++ src/br/com/zup/Exercicio1/Main.java
@@ -1,5 +1,8 @@
 package br.com.zup.Exercicio1;
 
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class Main {
 
     public static void main(String[] args) {
